6fd80d21b8f1e5931f57b277c615f934fc8760fb
Bei <buyixiaobei@163.com>
2017-11-08 02:47:31 +0800

    #1-创建一个新的C++空项目，编译器是VS2017，UE版本为4.18.0.

---
+++ RuinVR/Source/RuinVR/RuinVRGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RuinVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUINVR_API ARuinVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
