a2f00a713eee8224deddc75707944e5ab50a47d6
Bei <buyixiaobei@163.com>
2017-11-08 14:36:11 +0800

    #4-在VRCharacter中的BeginPlay事件中实现Hand的生成，并创建继承Actor的RuinPlayerHand C++文件

---
+++ RuinVR/Source/RuinVR/Player/RuinPlayerHand.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RuinPlayerHand.h"
+
+
+// Sets default values
+ARuinPlayerHand::ARuinPlayerHand()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARuinPlayerHand::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARuinPlayerHand::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
