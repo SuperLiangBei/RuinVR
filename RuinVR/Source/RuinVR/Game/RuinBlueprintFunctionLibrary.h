8581b981633c5ec01fbf68c6672b8ec90f5bfc26
Bei <buyixiaobei@163.com>
2017-11-11 00:52:33 +0800

    #12-实现Hand的基本动画（握拳）和骨骼的设置，增加RuinBlueprintFunctionLibrary蓝图函数库（C++）

---
+++ RuinVR/Source/RuinVR/Game/RuinBlueprintFunctionLibrary.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "RuinBlueprintFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUINVR_API URuinBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
